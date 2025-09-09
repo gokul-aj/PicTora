9e42c90f8fc05370f491b6b4a2ab91ac32aa86a9
gokul-aj <gokulprob@gmail.com>
2025-09-09 13:48:32 +0530

    Made seperate visibilty in navbar for users and non users

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {assets} from '../assets/assets'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 const Navbar = () => {
+  const[user, setUser]=useState(null);
+
+  const navigate = useNavigate()
   return (
-    <div>
+    <div className='flex items-center justify-between py-4'>
     <Link to='/'>
         <img src={assets.logo} className='w-28 sm:w-32 lg:w-40 pt-4 cursor-pointer'/>
         </Link>
+
+        <div>
+        {user ? 
+          <div></div> :  
+          <div className='flex items-center gap-2 sm:gap-5'>
+
+            <p onClick={()=> navigate('/buy')} className='cursor-pointer'>pricing</p>
+            <button className='bg-zinc-800 cursor-pointer text-white px-7 py-2 sm:px-10 text-sm rounded-full'>Login</button>
+          </div>
+          }
+        
+         
+        </div>
+
     </div>
   )
 }
