a5a605978cee9e6d341e951a0d5eecf8d6fa1458
gokul-aj <gokulprob@gmail.com>
2025-11-05 14:11:56 +0530

    Completed testimonials component

--- client/src/Components/Testimonials.jsx
+++ client/src/Components/Testimonials.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { testimonialsData } from '../assets/assets'
+import { assets, testimonialsData } from '../assets/assets'
 
 const Testimonials = () => {
   return (
@@ -9,8 +9,21 @@ const Testimonials = () => {
     <p className='text-gray-500 mb-12'>What Our Users Are Saying </p>
 
     <div className='flex flex-wrap gap-6 '>
-{testimonialsData.map((testimonial, index)=>(
-    <div key={index }></div>
+{testimonialsData.map((testimonail, index)=>(
+    <div key={index } className='bg-white/20 p-12 rounded-lg shadow-md order w-80 m-auto cursor-pointer hover:scale-[1.02] transition-all'>
+      <div className='flex flex-col items-center'>
+          <img src={testimonail.image} className='rounded-full w-14'/>
+          <h2 className='text-xl font-semibold mt-3 '>{testimonail.name}</h2>
+          <p className='text-gray-500 mb-4'>{testimonail.role}</p>
+          <div className='flex mb-4'>
+            {Array(testimonail.stars).fill().map((item,index)=>(
+                <img key={index} src={assets.rating_star}/>
+            ))}
+          </div>
+          <p className='text-center text-sm text-gray-600'>{testimonail.text}</p>
+      </div>
+
+    </div>
 ))}
     </div>
 
