38d19de70e651b80d0d21abc9ebc586c6c0f34dd
gokul-aj <gokulprob@gmail.com>
2025-11-07 13:17:42 +0530

    Completed the Home Page.

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className='flex items-center justify-between gap-4 py-3 mt-20 '>
     <img src={assets.logo} width={150}/>
-    <p className='flex-1 border-l border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden'>Copyright@Pictora.com All Rights Reserved.</p>
+    <p className='flex-1 border-l border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden'>Copyright @ Pictora.com All Rights Reserved.</p>
     <div className='flex gap-2.5'>
       <img src={assets.facebook_icon} width={35}/>
       <img src={assets.twitter_icon} width={35}/>
