ee84d8ba971926ad5f0fca575cbf30827d7101be
gokul-aj <gokulprob@gmail.com>
2025-11-07 13:34:50 +0530

    Started working on result page

--- client/src/Pages/Result.jsx
+++ client/src/Pages/Result.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Result = () => {
   return (
-    <div>Result</div>
+    <form>
+    <div>
+
+      <div className='relative'>
+        <img src={assets.sample_img_1} className='max-w-sm rounded' />
+        <span className='absolute bottom-0 left-0 h-1 bg-blue-500 w-full transition-all duration-[10s]' />
+      </div>
+      <p>Loading.....</p>
+    </div>
+    <div>
+      <input type='text'/>
+    </div>
+    </form>
   )
 }
 
