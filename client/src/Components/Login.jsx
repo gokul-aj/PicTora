e0c6f71f2aba4f804943834e7540f522f90cb173
gokul-aj <gokulprob@gmail.com>
2025-11-21 15:26:43 +0530

    Completed FrontEnd

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react'
 import {assets} from '../assets/assets'
 import { AppContext } from '../Context/AppContext'
+import { motion } from "motion/react"
 const Login = () => {
 
 const [state, setState] = useState('Login')
@@ -12,9 +13,14 @@ return ()=>{
 }
 },[])
   return (
-    <div className='absolute top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center'>
+    <div className='fixed absolute top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center'>
         
-        <form className='relative bg-white p-10 rounded-xl text-slate-500'> 
+        <motion.form className='relative bg-white p-10 rounded-xl text-slate-500'
+        initial={{opacity: 0.2, y:50}}
+        transition={{duration:0.3}}
+        whileInView={{opacity:1,y:0}}
+        viewport={{once:true}}
+        > 
         <h1 className='text-center text-2xl text-neutral-700 font-medium'>{state}</h1>
         <p className='text-sm'>Welcome back! Please {state} to Continue.</p>
         {state !== 'Login' &&  <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-5'>
@@ -41,7 +47,7 @@ return ()=>{
          <p className='mt-5 text-center '>Have an account? <span className='text-blue-600 cursor-pointer' onClick={()=>setState('Login')}>Login</span></p>}
 
          <img onClick={()=> setShowLogin(false)} src={assets.cross_icon} alt='' className='absolute top-5 right-5 cursor-pointer'/>
-        </form>
+        </motion.form>
     </div>
   )
 }
